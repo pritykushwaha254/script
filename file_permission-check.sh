b548dded56e634eedff3dfb24c317bb524a09256
Ubuntu <ubuntu@ip-172-31-8-98.ap-south-1.compute.internal>
2024-12-09 04:13:01 +0000

    First commit

---
+++ file_permission-check.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+<<comment
+Script to check file is redable, writable or executable
+comment
+
+file=$1
+
+if [ -r "$file" ]; then
+	echo "$file is redable"
+else 
+	echo "The file $file is not readable"
+
+fi
+
+if [ -w "$file" ]; then
+	echo "The file $file is writeable "
+else 
+	echo "The file $file is not Writable" 
+fi
+
+if [ -x "$file" ]; then
+	echo "The file $file is executable"
+else
+       echo 	"The file $file is not executable"
+fi
+
+
